db2df81ed6c61652aa38535037cf5e5e59f58c29
Mradul <143731437+mrrdl@users.noreply.github.com>
2025-06-28 04:19:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
+
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from inference import (
     predict_face_shape,
     predict_pose_measurements,
-    predict_skin_tone
+    generate_fashion_prompt,
+    get_recommmendation,
+    get_imgid,
+    generate_url,
+    fetch_image_from_url,
+    image_to_base64,
+    get_image_json
 )
 import uvicorn
 
@@ -27,15 +34,6 @@ async def face_shape_api(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.post("/predict/skin-tone")
-async def skin_tone_api(file: UploadFile = File(...)):
-    try:
-        image_bytes = await file.read()
-        result = predict_skin_tone(image_bytes)
-        return {"skin_tone": result}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
 @app.post("/predict/pose-measurement")
 async def pose_measurement_api(file: UploadFile = File(...)):
     try:
@@ -48,15 +46,40 @@ async def pose_measurement_api(file: UploadFile = File(...)):
 @app.post("/predict/all")
 async def predict_all(file: UploadFile = File(...)):
     image_bytes = await file.read()
-
     try:
         face= predict_face_shape(image_bytes)
-        skin = predict_skin_tone(image_bytes)
         pose = predict_pose_measurements(image_bytes)
+
         return {
             "face_shape": face,
-            'skin_tone': skin,
-            **pose
+            **pose,
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/get-img")
+async def get_id(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    try:
+        face= predict_face_shape(image_bytes)
+        pose = predict_pose_measurements(image_bytes)
+
+        data= {
+            "face_shape": face,
+            **pose,
+        }
+        prompt = generate_fashion_prompt(data)
+        recommendation = get_recommmendation(prompt)
+        img_id=get_imgid(recommendation)
+        img_url= generate_url(img_id)
+        img_json=get_image_json(img_url)
+        return{
+            "img_json":img_json,
+            "img_id": str(img_id),
+            "img_url": img_url,
+            "prompt": prompt,
+            "recommendation": recommendation,
+            "data":data,
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
